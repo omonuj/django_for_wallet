2f33acceb07c02bc014736b0069c621d5eec3258
jonah odoh <125787279+omonuj@users.noreply.github.com>
2025-02-18 08:12:57 +0100

    Update urls.py

--- wallet/urls.py
+++ wallet/urls.py
@@ -37,8 +37,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('accounts/', include('account.urls')),
     path('wallet/', include('ewallet.urls')),
 ]
